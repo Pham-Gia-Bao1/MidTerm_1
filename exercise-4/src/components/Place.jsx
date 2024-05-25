25cafecbff0eca3db537793e7ebcf6f28a1a1b07
PhamGiaBao <phamgiabao123abc@gmail.com>
2024-05-25 09:46:44 +0700

    finished exercise 4

---
+++ exercise-4/src/components/Place.jsx
@@ -0,0 +1,10 @@
+export default function Place({data}) {
+  return (
+    <li key={data.id} className="place-item">
+      <button>
+        <img src={data.image.src} alt={data.image.alt}/>
+        <h3>{data.title}</h3>
+      </button>
+    </li>
+  );
+}
